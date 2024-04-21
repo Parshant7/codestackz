cc7753b97c9196326b2bc9ff17e485e45e43b360
Balwinder singh <balwinder89405@gmail.com>
2024-04-21 20:12:11 +0530

    orange to white main  color changed

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -17,10 +17,10 @@ const Carousel = () => {
                         </div>
                     </div>
                     <div className="col-md-12 col-lg-6 p-0 my-auto d-none d-lg-block ">
-                        <div className="mx-auto ms-lg-auto me-lg-5 p-md-4" style={{ width: "70%", color: "white" }}>
+                        <div className="mx-auto ms-lg-auto me-lg-5 p-md-4" style={{ width: "70%", color: "black" }}>
                             <h1 className='fw-bold mb-4'>Web Designing and Mobile App Development Company</h1>
                             <p className='fw-bold fs-5 mb-xl-5 mb-lg-3 d-none d-sm-block'>Embrace innovation and elevate your company's trajectory with our specialized development solutions.</p>
-                            <Link className='btn btn-lg btn-success border rounded-5 px-xl-5 px-lg-4 bg-white' style={{ color: "#FE7A36 " }} to={'/contactus'}>Seek a Consultation</Link>
+                            <Link className='btn btn-lg btn-success border rounded-5 px-xl-5 px-lg-4 bg-primary' style={{ color: "white " }} to={'/contactus'}>Seek a Consultation</Link>
                         </div>
                     </div>
                     <div className="col-lg-6 p-0 d-none d-lg-block ">
